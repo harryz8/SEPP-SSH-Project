bbf273224c7444a8f12457b0bae971b6c826f8c1
harryz8 <153006330+harryz8@users.noreply.github.com>
2024-11-26 14:03:48 +0000

    Update README.md for project to specify java version and added a template JUnit test

---
+++ project/src/test/java/com/sshgroup/ssh_fridge_contents_tracker/MainApplicationTests.java
@@ -0,0 +1,10 @@
+package com.sshgroup.ssh_fridge_contents_tracker;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+public class MainApplicationTests {
+    @Test
+    void demoTestMethod() {
+        assertTrue(true);
+    }
+}
