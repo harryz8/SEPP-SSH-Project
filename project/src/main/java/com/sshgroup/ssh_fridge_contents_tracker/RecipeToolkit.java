a259269cfc944d53f3300773fcd999f1207fbfb2
harryz8 <hzoeftig@gmail.com>
2024-11-28 08:27:12 +0000

    added Hibernate to gradle

--- project/src/main/java/com/sshgroup/ssh_fridge_contents_tracker/RecipeToolkit.java
+++ project/src/main/java/com/sshgroup/ssh_fridge_contents_tracker/RecipeToolkit.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 public class RecipeToolkit {
     public static ArrayList<Recipe> sortByPriceOfRemainingItems(ArrayList<Recipe> recipeList) {
         // TODO: implement here
+        // Price of items can be looked up from the sql database
         return recipeList;
     }
     public static ArrayList<Recipe> filterByCategory(ArrayList<Recipe> recipeList, Categories category) {
