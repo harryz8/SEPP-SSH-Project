628a9ab2af854c354fa8b7948981b119a2ab7ad7
Khaled <kxa284@student.bham.ac.uk>
2024-11-28 15:14:50 +0000

    initial recipe class

--- project/src/main/java/com/sshgroup/ssh_fridge_contents_tracker/Recipe.java
+++ project/src/main/java/com/sshgroup/ssh_fridge_contents_tracker/Recipe.java
@@ -1,4 +1,35 @@
 package com.sshgroup.ssh_fridge_contents_tracker;
 
+import java.util.List;
+
 public class Recipe {
+    int id;
+    String name;
+    String instruction;
+    private List<String> ingredients;
+
+    public Recipe(int id, String name, String instruction, List<String> ingredients) {
+        this.id = id;
+        this.name = name;
+        this.instruction = instruction;
+        this.ingredients = ingredients;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getInstruction() {
+        return instruction;
+    }
+
+    public List<String> getIngredients() {
+        return ingredients;
+    }
+
+
 }
