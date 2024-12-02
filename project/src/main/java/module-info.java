831927684049b4b277d14d51ff4401863d75812d
harryz8 <harry.zoeftig@btinternet.com>
2024-12-02 11:27:47 +0000

    webScraper for ocado

--- project/src/main/java/module-info.java
+++ project/src/main/java/module-info.java
@@ -2,6 +2,9 @@ module com.sshgroup.ssh_fridge_contents_tracker {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires jdk.unsupported.desktop;
+    requires java.rmi;
+    requires org.hibernate.orm.core;
 
 
     opens com.sshgroup.ssh_fridge_contents_tracker to javafx.fxml;
