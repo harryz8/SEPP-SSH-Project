4e4fdf8bea0da43804d56bc0e9cb810c8589b2f1
tomnourish <tomnourish@gmail.com>
2024-12-03 11:58:08 +0000

    I have created the basic scanner and have begun storing the data. I will wait until the next meeting to see how the database is implemented

--- project/src/main/java/com/sshgroup/ssh_fridge_contents_tracker/RecipeCreator.java
+++ project/src/main/java/com/sshgroup/ssh_fridge_contents_tracker/RecipeCreator.java
@@ -1,5 +1,31 @@
 package com.sshgroup.ssh_fridge_contents_tracker;
 
+import java.util.Scanner;
+import java.util.ArrayList;
 public class RecipeCreator {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the recipe name: ");
+        String recipe_name = scanner.nextLine();
+
+        System.out.print("Enter recipe instructions: ");
+        String recipe_instructions = scanner.nextLine();
+
+        ArrayList<String> ingredient_name = new ArrayList<String>();
+        ArrayList<Integer> amount_needed = new ArrayList<Integer>();
+
+        while(true) {
+            System.out.print("Enter ingredient name (or done): ");
+            String temp = scanner.nextLine();
+            if (temp == "done") {
+                break;
+            } else {
+                ingredient_name.add(temp);
+            }
+            System.out.print("Enter amount required: ");
+            amount_needed.add(scanner.nextInt());
+        }
+
+    }
 }
