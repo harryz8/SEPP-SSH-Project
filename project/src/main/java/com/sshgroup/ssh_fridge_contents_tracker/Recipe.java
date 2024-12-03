79ae0ae280e710271fbf37cfad676ecccc511215
PhobDarl <pdarli@icloud.com>
2024-12-03 11:36:15 +0000

    Added the sortByPrice and sortByCategories methods. Both need changing based on database methods I haven't got yet.

--- project/src/main/java/com/sshgroup/ssh_fridge_contents_tracker/Recipe.java
+++ project/src/main/java/com/sshgroup/ssh_fridge_contents_tracker/Recipe.java
@@ -1,4 +1,33 @@
 package com.sshgroup.ssh_fridge_contents_tracker;
 
+import java.util.ArrayList;
+
 public class Recipe {
+    private int id;
+    private String name;
+    private String instructions;
+
+    public int getID(){
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getInstructions(){
+        return instructions;
+    }
+
+    public ArrayList<Ingredient> getIngredientList(){
+         return null;
+    }
+
+    public int getQuantity(int ingredientID){
+        return 0;
+    }
+
+    public static Categories getCategory(){
+        return null;
+    }
 }
