dbe1c1f7357a3b9edf034d49bd1184911bbf02d6
phobdarl <pdarli@icloud.com>
2024-12-09 17:14:24 +0000

    added to dbAccess and used them in

--- project/src/main/java/com/sshgroup/ssh_fridge_contents_tracker/RecipeToolkit.java
+++ project/src/main/java/com/sshgroup/ssh_fridge_contents_tracker/RecipeToolkit.java
@@ -1,5 +1,7 @@
 package com.sshgroup.ssh_fridge_contents_tracker;
 
+import org.hibernate.SessionFactory;
+
 import java.lang.reflect.Array;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -10,7 +12,7 @@ import java.util.Map;
 
 public class RecipeToolkit {
 
-    SessionFactory session = DatabaseAccess.setup()
+    SessionFactory session = DatabaseAccess.setup();
 
     public static ArrayList<Recipe> sortByPriceOfRemainingItems(ArrayList<Recipe> recipeList) {
         ArrayList<Ingredient> ingList = new ArrayList<>();
@@ -19,7 +21,7 @@ public class RecipeToolkit {
         // Iterate through the recipe list
         for (Recipe r : recipeList){
             double temp = 0.0;
-            Int recipeID = r.getID();
+            Integer recipeID = r.getId();
             // get list of ingredients and loop through
             ingList = r.getIngredientList();
             for (Ingredient i : ingList){
@@ -47,76 +49,14 @@ public class RecipeToolkit {
         return sortedList;
     }
 
-    /**
-     * A function that takes the name of an ingredient needed and the minimum quantity needed and uses webscraping techniques to find the cheapest item with the required quantity and returns the price of that item
-     * @param ingredientName a string of the name of the ingredient required to find the price for
-     * @param quantityNeeded the minimum quantity of the ingredient required. If item does not have a specified quantity, pass -1
-     * @return the {@link com.sshgroup.ssh_fridge_contents_tracker.PriceQuantity} of the cheapest specified ingredient that is greater than the quantity needed. Null will be returned in the error case
-     */
-    public static PriceQuantity getCheapestIngredient(String ingredientName, double quantityNeeded) {
-        PriceQuantity minItem = CacheMap.cache.get(ingredientName, String.valueOf(quantityNeeded));
-        if (minItem != null) {
-            // System.out.println("Yay!");
-            return minItem;
-        }
-        ArrayList<PriceQuantity> ocadoItems = new ArrayList<>();
-        try {
-            //Ocado Scraper
-            try {
-                URL url = new URL("https://www.ocado.com/search?entry=" + ingredientName);
-                WebScraper ws = new WebScraper(url);
-                String body = ws.getWebpageHtml();
-                if (body == null) {
-                    throw new NoItemsException("Ocado Scraper returned valid no results");
-                }
-                String el2 = WebScraper.getElementByID(body, "div", "main-column", "class");
-                if (el2 == null) {
-                    throw new NoItemsException("Ocado Scraper returned valid no results");
-                }
-                String element = WebScraper.getElementByID(el2, "ul", "fops fops-regular fops-shelf", "class");
-                if (element == null) {
-                    throw new NoItemsException("Ocado Scraper returned valid no results");
-                }
-                ArrayList<String> LIs = WebScraper.getUnorderedListItems(element);
-                for (String li : LIs) {
-                    String li2 = null;
-                    li2 = WebScraper.getElementByID(li, "div", "fop-item", "class");
-                    if (li2 == null) {
-                        li2 = WebScraper.getElementByID(li, "div", "fop-item fop-item-offer", "class");
-                        if (li2 == null) {
-                            continue;
-                        }
-                    }
-                    String linkElement = WebScraper.getElement(li2, "a");
-                    if (linkElement == null) {
-                        continue;
-                    }
-                    String link = WebScraper.getParameterValue(linkElement, "href");
-                    if (link == null) {
-                        continue;
-                    }
-                    try {
-                        OcadoPriceQuantity newItem = new OcadoPriceQuantity("https://www.ocado.com" + link);
-                        ocadoItems.add(newItem);
-                    }
-                    catch (ItemNotFoundException e) {
-                        System.out.println(e.toString());
-                        continue;
-                    }
-                }
-                if (ocadoItems.isEmpty()) {
-                    throw new NoItemsException("Ocado Scraper returned valid no results");
-                }
-            }
-            catch (NoItemsException e) {
-                System.out.println("Error: "+ e.toString());
-            }
-            if (quantityNeeded > 0) {
-                ocadoItems.removeIf(s -> s.getQuantity() < quantityNeeded);
-            }
-            PriceQuantity minOcado = PriceQuantity.getMinimumPrice((ArrayList<PriceQuantity>) ocadoItems);
-            if (minOcado == null) {
-                return null;
+
+    public static ArrayList<Recipe> filterByCategory(ArrayList<Recipe> recipeList, Categories category) {
+        // declare new list
+        ArrayList<Recipe> newList = new ArrayList<>();
+        // loop through original list and only add to the new list if the category matches
+        for(Recipe r : recipeList){
+            if (Recipe.getCategory().equals(category)){
+                newList.add(r);
             }
             CacheMap.cache.put(ingredientName, String.valueOf(quantityNeeded), minOcado);
             return minOcado;
