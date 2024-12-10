9361e39ca37889d3cb3d29e77c860d619f2a8a38
harryz8 <harry.zoeftig@btinternet.com>
2024-12-10 13:06:19 +0000

    added more tests

--- project/src/test/java/com/sshgroup/ssh_fridge_contents_tracker/MainApplicationTests.java
+++ project/src/test/java/com/sshgroup/ssh_fridge_contents_tracker/MainApplicationTests.java
@@ -1,13 +1,9 @@
 package com.sshgroup.ssh_fridge_contents_tracker;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-
 import java.net.MalformedURLException;
 import java.net.URL;
-
 import static org.junit.jupiter.api.Assertions.*;
-
 
 public class MainApplicationTests {
     @Test
@@ -125,4 +121,61 @@ public class MainApplicationTests {
             System.out.println("Error: "+e.toString());
         }
     }
+    @Test
+    void testStringLocation() {
+        String test1 = "";
+        String test2 = "hello world";
+        String test3 = "whatisthisString123";
+        assertEquals(null, StringToolkit.getPositions(test1, "hello"));
+        assertEquals(null, StringToolkit.getPositions(test1, ""));
+        StringLocation sl1 = StringToolkit.getPositions(test2, "hello");
+        assertEquals(0, sl1.getFirstPos());
+        assertEquals(4, sl1.getLastPos());
+        StringLocation sl2 = StringToolkit.getPositions(test2, "world");
+        assertEquals(6, sl2.getFirstPos());
+        assertEquals(10, sl2.getLastPos());
+        StringLocation sl3 = StringToolkit.getPositions(test3, "this");
+        assertEquals(6, sl3.getFirstPos());
+        assertEquals(9, sl3.getLastPos());
+        StringLocation sl4 = StringToolkit.getPositions(test3, "1");
+        assertEquals(16, sl4.getLastPos());
+        assertEquals(16, sl4.getFirstPos());
+        StringLocation sl5 = StringToolkit.getPositions(test3, "search");
+        assertEquals(null, sl5);
+    }
+    @Test
+    void testContainsDigits() {
+        assertTrue(StringToolkit.containsDigits("hello123hello"));
+        assertTrue(StringToolkit.containsDigits("what1what"));
+        assertTrue(StringToolkit.containsDigits("8 is the best number"));
+        assertTrue(StringToolkit.containsDigits("Why do you like the number 7"));
+        assertFalse(StringToolkit.containsDigits("Eighty Three"));
+        assertFalse(StringToolkit.containsDigits(""));
+        assertFalse(StringToolkit.containsDigits(null));
+        assertTrue(StringToolkit.containsDigits("8"));
+        assertTrue(StringToolkit.containsDigits(" 8j"));
+        assertTrue(StringToolkit.containsDigits("z8 "));
+    }
+    @Test
+    void getElementByIdNoCloseTagTest() {
+        String incElement = "<div class=\"test\"><p>Hello HTML</p><a href=\"Alink.com\">Some Link</a></div>";
+        assertEquals("<a href=\"Alink.com\">", WebScraper.getElementByIDNoCloseTag(incElement, "a", "Alink.com", "href"));
+        assertNull(WebScraper.getElementByIDNoCloseTag(null, "a", "Alink.com", "href"));
+        assertNull(WebScraper.getElementByIDNoCloseTag(incElement, null, "Alink.com", "href"));
+        assertNull(WebScraper.getElementByIDNoCloseTag(incElement, "a", null, "href"));
+        assertNull(WebScraper.getElementByIDNoCloseTag(incElement, "a", "Alink.com", null));
+        assertNull(WebScraper.getElementByIDNoCloseTag("", "a", "Alink.com", "href"));
+        assertNull(WebScraper.getElementByIDNoCloseTag(incElement, "", "Alink.com", "href"));
+        assertNull(WebScraper.getElementByIDNoCloseTag(incElement, "a", "", "href"));
+        assertNull(WebScraper.getElementByIDNoCloseTag(incElement, "a", "Alink.com", ""));
+        assertEquals("<div class=\"test\">", WebScraper.getElementByIDNoCloseTag(incElement, "div", "test", "class"));
+        assertNull(WebScraper.getElementByIDNoCloseTag(incElement, "q", "test", "class"));
+        assertNull(WebScraper.getElementByIDNoCloseTag(incElement, "div", "red", "class"));
+        assertNull(WebScraper.getElementByIDNoCloseTag(incElement, "div", "test", "id"));
+    }
+//    @Test
+//    void getElementById() {
+//        String incElement = "<div class=\"test\"><p>Hello HTML</p><a href=\"Alink.com\">Some Link</a></div>";
+//
+//    }
 }
