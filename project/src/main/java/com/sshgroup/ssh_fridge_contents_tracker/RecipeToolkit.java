ec603e2894fd70fae1692e9e448dcc3fe6437c9c
harryz8 <harry.zoeftig@btinternet.com>
2024-12-11 09:34:22 +0000

    rewrote CacheMap load code

--- project/src/main/java/com/sshgroup/ssh_fridge_contents_tracker/RecipeToolkit.java
+++ project/src/main/java/com/sshgroup/ssh_fridge_contents_tracker/RecipeToolkit.java
@@ -52,90 +52,88 @@ public class RecipeToolkit {
         }
         return sortedList;
     }
+    public static ArrayList<Recipe> filterByCategory(ArrayList<Recipe> recipeList, Category category) {
+        // TODO: implement here
+        return recipeList;
+    }
 
-    public  ArrayList<Recipe> filterByCategory(ArrayList<Recipe> recipeList, Category category) {
-        // declare new list
-        ArrayList<Recipe> newList = new ArrayList<>();
-        // loop through original list and only add to the new list if the category matches
-        for(Recipe r : recipeList){
-            if (dbAccess.getCategory(r).equals(category.getCategory_id())){
-                newList.add(r);
-            }
+    /**
+     * A function that takes the name of an ingredient needed and the minimum quantity needed and uses webscraping techniques to find the cheapest item with the required quantity and returns the price of that item
+     * @param ingredientName a string of the name of the ingredient required to find the price for
+     * @param quantityNeeded the minimum quantity of the ingredient required. If item does not have a specified quantity, pass -1
+     * @return the {@link com.sshgroup.ssh_fridge_contents_tracker.PriceQuantity} of the cheapest specified ingredient that is greater than the quantity needed. Null will be returned in the error case
+     */
+    public static PriceQuantity getCheapestIngredient(String ingredientName, double quantityNeeded) {
+        PriceQuantity minItem = CacheMap.cache.get(ingredientName, String.valueOf(quantityNeeded));
+        if (minItem != null) {
+//            System.out.println("Yay!");
+            return minItem;
         }
-        return newList;
-    }
-
-public static PriceQuantity getCheapestIngredient(String ingredientName, double quantityNeeded) {
-    PriceQuantity minItem = CacheMap.cache.get(ingredientName, String.valueOf(quantityNeeded));
-    if (minItem != null) {
-        // System.out.println("Yay!");
-        return minItem;
-    }
-    ArrayList<PriceQuantity> ocadoItems = new ArrayList<>();
-    try {
-        //Ocado Scraper
+        ArrayList<PriceQuantity> ocadoItems = new ArrayList<>();
         try {
-            URL url = new URL("https://www.ocado.com/search?entry=" + ingredientName);
-            WebScraper ws = new WebScraper(url);
-            String body = ws.getWebpageHtml();
-            if (body == null) {
-                throw new NoItemsException("Ocado Scraper returned valid no results");
-            }
-            String el2 = WebScraper.getElementByID(body, "div", "main-column", "class");
-            if (el2 == null) {
-                throw new NoItemsException("Ocado Scraper returned valid no results");
-            }
-            String element = WebScraper.getElementByID(el2, "ul", "fops fops-regular fops-shelf", "class");
-            if (element == null) {
-                throw new NoItemsException("Ocado Scraper returned valid no results");
-            }
-            ArrayList<String> LIs = WebScraper.getUnorderedListItems(element);
-            for (String li : LIs) {
-                String li2 = null;
-                li2 = WebScraper.getElementByID(li, "div", "fop-item", "class");
-                if (li2 == null) {
-                    li2 = WebScraper.getElementByID(li, "div", "fop-item fop-item-offer", "class");
+            //Ocado Scraper
+            try {
+                URL url = new URL("https://www.ocado.com/search?entry=" + ingredientName);
+                WebScraper ws = new WebScraper(url);
+                String body = ws.getWebpageHtml();
+                if (body == null) {
+                    throw new NoItemsException("Ocado Scraper returned valid no results");
+                }
+                String el2 = WebScraper.getElementByID(body, "div", "main-column", "class");
+                if (el2 == null) {
+                    throw new NoItemsException("Ocado Scraper returned valid no results");
+                }
+                String element = WebScraper.getElementByID(el2, "ul", "fops fops-regular fops-shelf", "class");
+                if (element == null) {
+                    throw new NoItemsException("Ocado Scraper returned valid no results");
+                }
+                ArrayList<String> LIs = WebScraper.getUnorderedListItems(element);
+                for (String li : LIs) {
+                    String li2 = null;
+                    li2 = WebScraper.getElementByID(li, "div", "fop-item", "class");
                     if (li2 == null) {
+                        li2 = WebScraper.getElementByID(li, "div", "fop-item fop-item-offer", "class");
+                        if (li2 == null) {
+                            continue;
+                        }
+                    }
+                    String linkElement = WebScraper.getElement(li2, "a");
+                    if (linkElement == null) {
+                        continue;
+                    }
+                    String link = WebScraper.getParameterValue(linkElement, "href");
+                    if (link == null) {
+                        continue;
+                    }
+                    try {
+                        OcadoPriceQuantity newItem = new OcadoPriceQuantity("https://www.ocado.com" + link);
+                        ocadoItems.add(newItem);
+                    }
+                    catch (ItemNotFoundException e) {
+                        System.out.println(e.toString());
                         continue;
                     }
                 }
-                String linkElement = WebScraper.getElement(li2, "a");
-                if (linkElement == null) {
-                    continue;
-                }
-                String link = WebScraper.getParameterValue(linkElement, "href");
-                if (link == null) {
-                    continue;
-                }
-                try {
-                    OcadoPriceQuantity newItem = new OcadoPriceQuantity("https://www.ocado.com" + link);
-                    ocadoItems.add(newItem);
-                }
-                catch (ItemNotFoundException e) {
-                    System.out.println(e.toString());
-                    continue;
+                if (ocadoItems.isEmpty()) {
+                    throw new NoItemsException("Ocado Scraper returned valid no results");
                 }
             }
-            if (ocadoItems.isEmpty()) {
-                throw new NoItemsException("Ocado Scraper returned valid no results");
+            catch (NoItemsException e) {
+                System.out.println("Error: "+ e.toString());
             }
+            if (quantityNeeded > 0) {
+                ocadoItems.removeIf(s -> s.getQuantity() < quantityNeeded);
+            }
+            PriceQuantity minOcado = PriceQuantity.getMinimumPrice((ArrayList<PriceQuantity>) ocadoItems);
+            if (minOcado == null) {
+                return null;
+            }
+            CacheMap.cache.put(ingredientName, String.valueOf(quantityNeeded), minOcado);
+            return minOcado;
         }
-        catch (NoItemsException e) {
-            System.out.println("Error: "+ e.toString());
-        }
-        if (quantityNeeded > 0) {
-            ocadoItems.removeIf(s -> s.getQuantity() < quantityNeeded);
-        }
-        PriceQuantity minOcado = PriceQuantity.getMinimumPrice((ArrayList<PriceQuantity>) ocadoItems);
-        if (minOcado == null) {
+        catch (MalformedURLException e) {
+            System.out.println("Unable to connect to web store. Error: "+e.toString());
             return null;
         }
-        CacheMap.cache.put(ingredientName, String.valueOf(quantityNeeded), minOcado);
-        return minOcado;
     }
-    catch (MalformedURLException e) {
-        System.out.println("Unable to connect to web store. Error: "+e.toString());
-        return null;
-    }
-}
 }
